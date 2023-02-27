6279da84c8ff79defa4cb9a0cb961f6af68767f5
cian murray <g00358011@atu.ie>
2023-02-27 15:20:06 +0000

    Prompt user for length

--- src/main/java/ie/atu/ie/atu/week5/ShapeApp.java
+++ src/main/java/ie/atu/ie/atu/week5/ShapeApp.java
@@ -1,4 +1,13 @@
 package ie.atu.ie.atu.week5;
 
+import java.util.Scanner;
+
 public class ShapeApp {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter the Width of the Rectangle");
+        Scanner input = new Scanner(System.in);
+        double length = input.nextDouble();
+        System.out.println("You entered : " + length);
+    }
 }
